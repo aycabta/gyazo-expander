6b6a50c6c7046b720dacf07477d60656d4847858
Code Ass <aycabta@gmail.com>
2014-06-02 18:15:51 +0900

    initial commit

---
+++ web.rb
@@ -0,0 +1,24 @@
+require 'bundler'
+require 'sinatra'
+
+post '/nicothumb' do
+  content_type :text
+  json = JSON.parse(request.body.read)
+  if not json["events"].nil?
+    json["events"].map do |e|
+      if e["message"] && e["message"]["text"] =~ %r`^http://gyazo\.com/\w+$`
+        message = e["message"]["text"]
+        agent = Mechanize.new
+        agent.user_agent = "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:26.0) Gecko/20100101 Firefox/26.0"
+        agent.get(gyazo_url)
+        image_node = @agent.page.parser.xpath("//meta[@name='twitter:image']").first.attributes["content"]
+        if image_node
+          image_node.value
+        else
+          "\n"
+        end
+      end
+    end
+  end
+end
+
